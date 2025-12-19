2996087b8dff21018935bef82c3d5e614283d557
Mr Rehan <rehanmatloob43@gmail.com>
2025-12-19 15:07:03 +0500

    Snippetly Sync: Added Jws authentication middleware

---
+++ javascript/jws-authentication-middleware.js
@@ -0,0 +1,9 @@
+// Title: Jws authentication middleware
+// Language: javascript
+// Tags: none
+// Created: 12/6/2025
+// Snippet ID: 401b3df3-d932-48b7-a49a-05e6068f68cb
+
+export default function Loading() {
+  return null
+}
