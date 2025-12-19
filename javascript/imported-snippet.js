29995804af441aa30d16e55ccf7b6b9f2abaf880
Mr Rehan <rehanmatloob43@gmail.com>
2025-12-19 15:07:01 +0500

    Snippetly Sync: Updated Imported Snippet

--- javascript/imported-snippet.js
+++ javascript/imported-snippet.js
@@ -1,12 +1,27 @@
 // Title: Imported Snippet
 // Language: javascript
 // Tags: none
-// Created: 12/5/2025
-// Snippet ID: f684fefb-dab0-4157-a6be-27047b22932f
+// Created: 12/19/2025
+// Snippet ID: 79d4a95a-882a-41e3-862f-0b464fd2d501
 
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+# 🚀 Deployment & DevOps
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+[Docker](https://www.docker.com/)
+- Standard for containerization
+- Runs anywhere
+- Essential for scaling
+
+[Kubernetes](https://kubernetes.io/)
+- Orchestration for containers
+- Scales complex SaaS
+- Overkill for MVPs, perfect for growth
+
+ [Fly.io](https://fly.io/)
+- Deploy apps globally
+- Free tier available
+- Focused on edge deployment
+
+[Heroku](https://heroku.com/)
+- Easiest app deployment
+- Great for prototyping
+- Limited free tier now
