aedbbc65f5b866862e7f74086d2c627e2ae2d50c
Mr Rehan <rehanmatloob43@gmail.com>
2025-12-19 15:07:02 +0500

    Snippetly Sync: Added database sql connection Snippet

---
+++ javascript/database-sql-connection-snippet.js
@@ -0,0 +1,20 @@
+// Title: database sql connection Snippet
+// Language: javascript
+// Tags: none
+// Created: 12/5/2025
+// Snippet ID: aab1b822-f960-47e0-ba87-2100c7858ecb
+
+User-agent: Googlebot
+Allow: /
+
+User-agent: Bingbot
+Allow: /
+
+User-agent: Twitterbot
+Allow: /
+
+User-agent: facebookexternalhit
+Allow: /
+
+User-agent: *
+Allow: /
